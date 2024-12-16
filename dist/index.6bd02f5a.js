1248d9854f5cdd87139893e4fa02fae372a0701f
Harshad22902 <127301596+harshad-hussain@users.noreply.github.com>
2024-12-16 16:24:26 +0530

    push

---
+++ dist/index.6bd02f5a.js
@@ -0,0 +1,27 @@
+const parent = React.createElement("div", {
+    id: "parent"
+}, React.createElement("div", {
+    id: "Child"
+}, [
+    React.createElement("h1", {
+        id: "child 1"
+    }, "I m h1 tag"),
+    React.createElement("h1", {
+        id: "child 2"
+    }, "I m h2 tag")
+], React.createElement("div", {
+    id: "Child 2"
+}, [
+    React.createElement("h1", {
+        id: "child 3"
+    }, "I m h1 tag"),
+    React.createElement("h1", {
+        id: "child 4"
+    }, "I m h2 tag")
+])));
+console.log(parent);
+// const heading = React.createElement("h1",{},"Hello world from React!")
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(parent);
+
+//# sourceMappingURL=index.6bd02f5a.js.map
